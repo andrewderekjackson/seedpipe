bd640526cfec181332d0a7b24c4b7b080c0f515d
Andrew Jackson <andrewderekjackson@gmail.com>
2017-10-24 20:10:03 +1300

    better handling of exit codes

--- seedpipe/worker/downloader.py
+++ seedpipe/worker/downloader.py
@@ -149,11 +149,40 @@ class DownloaderThread(Thread):
 
     def complete(self, exit_code):
 
-        if exit_code == 0:
+       # 0      Success
+       # 1      Syntax or usage error
+       # 2      Protocol incompatibility
+       # 3      Errors selecting input/output files, dirs
+       # 4      Requested  action not supported: an attempt was made to manipulate 64-bit files on a platform
+       #        that cannot support them; or an option was specified that is supported by the client and not by the server.
+       # 5      Error starting client-server protocol
+       # 6      Daemon unable to append to log-file
+       # 10     Error in socket I/O
+       # 11     Error in file I/O
+       # 12     Error in rsync protocol data stream
+       # 13     Errors with program diagnostics
+       # 14     Error in IPC code
+       # 20     Received SIGUSR1 or SIGINT
+       # 21     Some error returned by waitpid()
+       # 22     Error allocating core memory buffers
+       # 23     Partial transfer due to error
+       # 24     Partial transfer due to vanished source files
+       # 25     The --max-delete limit stopped deletions
+       # 30     Timeout in data send/receive
+       # 35     Timeout waiting for daemon connection
+
+        if exit_code in [0]:
+            # success codes
             set_status(session, self.current_job, JOB_STATUS_POSTPROCESSING)
         else:
-            set_status(session, self.current_job, JOB_STATUS_FAILED
-                       )
+            # non-fatal codes
+            if exit_code in [20, 130]:
+                # we'll just retry latest
+                pass
+            else:
+                # fatal codes
+                set_status(session, self.current_job, JOB_STATUS_FAILED)
+
         self.current_job = None
         self.process = None
 
